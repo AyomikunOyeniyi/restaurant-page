f7a3d13f5ef6b2c26945cb37bf3af100b4e297d5
Ayomikun Oyeniyi <ayomikunoyeniyi07@gmail.com>
2024-02-10 05:20:53 -0500

    Create module to append content to div

---
+++ src/add-content.js
@@ -0,0 +1,19 @@
+import image from './food.jpg';
+
+export default function addContent() {
+    const container = document.querySelector('#content');
+    
+    const heading = document.createElement('h1');
+    heading.textContent = 'Welcome to Barati! Award winning restaurant of the seas!';
+    container.appendChild(heading);
+
+    const myImage = new Image();
+    myImage.src = image;
+    container.appendChild(myImage);
+
+    const text = document.createElement('p');
+    text.textContent = 'Barati is an amazing restaurant headed by Celebrity Chef AZ. We make good food if we do say so ourselves. Our 3 Michelin stars attest to that!';
+    container.appendChild(text);
+}
+
+
